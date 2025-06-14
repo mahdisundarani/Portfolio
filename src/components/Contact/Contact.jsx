dad6b37bac63ddb3c0e6c4af47ae97d9ccca06bc
Mahdi Sundarani <mahdi.sundarani@gmail.com>
2025-06-14 11:21:44 +0530

    edited Email contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_axbtt7a",  // Replace with your EmailJS Service ID
-        "template_1ziboq3",  // Replace with your EmailJS Template ID
+        "service_d0y53l4",  // Replace with your EmailJS Service ID
+        "template_4wn3p0e",  // Replace with your EmailJS Template ID
         form.current,
-        "Rz7W9pVF0HdDryNNL"  // Replace with your EmailJS Public Key
+        "ZNBR12yvxbRFoE0hg"  // Replace with your EmailJS Public Key
       )
       .then(
         () => {
